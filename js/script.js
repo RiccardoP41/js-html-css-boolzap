881d37007bca5b89ac49a0f71afe39d3e16a6b73
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-29 16:21:28 +0200

    mmm...

--- js/script.js
+++ js/script.js
@@ -1,59 +1,36 @@
 $(document).ready(function(){
 
-// *******CON INVIO********
-
-    var d = new Date();
-    var ore = d.getHours() + ":" + d.getMinutes();
-
-    $("#msg").keydown(spedisci);
-
-        function spedisci() {
-            if ( event.which == 13 || event.keycode == 13){
-// prendo il messaggio scritto nell'input
-            var valore = $("#msg").val();
-// clono il template
-            var chat = $(".template .messaggio").clone();
-// lo personalizzo
-            chat.append(valore);
-            chat.addClass("inviato");
-// lo aggiungo al dom
-            $(".chat").append(chat);
-
-
-
-
-            // setTimeout(risposta, 1000);
-            //
-            // function risposta() {
-            //     var ok = "ok";
-            //     var chat = $(".template .messaggio").clone();
-            //     chat.append(ok);
-            //     chat.addClass("ricevuto");
-            //     $(".chat").append(chat);
-            // }
+    $("#msg").keydown(function(){
+        if ( event.which == 13 || event.keycode == 13){
+            app();
         }
-    }// fine if
+    });
 
-// ************CON IL CLICK
-//     $("#send").click(invia);
-//
-//     function invia() {
-//         var valore = $("#msg").val();
-//         var chat = $(".template .messaggio").clone();
-//         chat.append(valore);
-//         chat.addClass("inviato");
-//         $(".chat").append(chat);
-// // risposta
-//         setTimeout(ricevi, 1000);
-//
-//         function ricevi() {
-//             var ok = "ok";
-//             var chat = $(".template .messaggio").clone();
-//             chat.append(ok);
-//             chat.addClass("ricevuto");
-//             $(".chat").append(chat);
-//         }
-//     }
+    $("#send").click(app);
+
+//FUNZIONI
+    function app() {
+        var d = new Date();                             //trovo l'orario con ore e minuti
+        var ore = d.getHours() + ":" + d.getMinutes();
+        var valore = $("#msg").val();                   //trovo il valore del contenuto dell'input
+        var chat = $(".template .messaggio").clone();   //clono il temeplate
+        chat.children(".text").text(valore);            //inserisco il testo e l'orario nei giusti contenitori
+        chat.children(".hours").text(ore);
+        chat.addClass("inviato");                       //aggiungo la classe giusta
+        $(".chat").append(chat);                        //"stampo" il msg nella chat
+        $("#msg").val("");                              //resetto lo spazio input dopo l'invio
+
+        setTimeout(risposta, 1000);
+
+        function risposta() {                                   //risposta
+            var ok = "ok";
+            var chat = $(".template .messaggio").clone();
+            chat.children(".text").text(ok);
+            chat.children(".hours").text(ore);
+            chat.addClass("ricevuto");
+            $(".chat").append(chat);
+        }
+    }
 
 // chiusura document.ready
 });
