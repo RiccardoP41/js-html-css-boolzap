bb2d1ff83abca268cf676b89d1929efb0e1f5b49
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-30 12:33:55 +0200

    aggiunta la classe active e fatte delle modifiche

--- js/script.js
+++ js/script.js
@@ -8,6 +8,23 @@ $(document).ready(function(){
 
     $("#send").click(app);
 
+    var random = [
+        "Ok!",
+        "Tarapìa tapiòco! Prematurata la supercazzola, o scherziamo?",
+        "Antani, come se fosse antani, anche per il direttore, la supercazzola con scappellamento",
+        "Questo è un discorso illativo",
+        "Qui si riscontra un chiaro intento mitopoietico",
+        "Hai scritto molte cose interessanti: potresti fare uno spicilegio",
+        "Sono un uomo d’affari, blinda la supercazzola prematurata,tarapia tapioca, tapioca, torapia, dollari, sterline, allaccia scarpa, scarpallaccia, dico d’albergo, ma tu?",
+        "No!",
+        "Boh...",
+        "Forse",
+        "Non ne sarei in grado",
+        "Blinda la supercazzola con scappellamento a sinistra e a destra come se fossero dei pentoloni.",
+        "Quintana, o setta! Intanto, trini la confraternita, pulitina!",
+    ];
+
+
 //FUNZIONI
     function app() {
         var d = new Date();                             //trovo l'orario con ore e minuti
@@ -17,20 +34,29 @@ $(document).ready(function(){
         chat.children(".text").text(valore);            //inserisco il testo e l'orario nei giusti contenitori
         chat.children(".hours").text(ore);
         chat.addClass("inviato");                       //aggiungo la classe giusta
+        chat.children(".triangolo").addClass("t-inviato") // Non importante
         $(".chat").append(chat);                        //"stampo" il msg nella chat
         $("#msg").val("");                              //resetto lo spazio input dopo l'invio
+        var posizione = $('.chat .messaggio:last-child').position();  //scroll
+        $('.chat').scrollTop(posizione.top);                          // "all credits gone to Claudio Cenghialta"
 
         setTimeout(risposta, 1000);
 
         function risposta() {                                   //risposta
-            var ok = "ok";
+            var tapioca = numeroRandom( 0, (random.length-1));  //risp random
+            var supercazzola = (random[tapioca]);               //risp random
             var chat = $(".template .messaggio").clone();
-            chat.children(".text").text(ok);
+            chat.children(".text").text(supercazzola);
             chat.children(".hours").text(ore);
             chat.addClass("ricevuto");
+            chat.children(".triangolo").addClass("t-ricevuto") // Non importante
             $(".chat").append(chat);
+            $('.chat').scrollTop(posizione.top);      // "all credits gone to Claudio Cenghialta"
         }
     }
 
+    function numeroRandom(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
 // chiusura document.ready
 });
